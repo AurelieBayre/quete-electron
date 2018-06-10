b18dc9121da5184587ccb4969b6b6ab4cbd6e76f
Aurelie Bayre <a.bayre@gmail.com>
2018-06-10 20:18:59 +0200

    feat: complete quest Electron

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const today = new Date()
+const presentTime = today.toLocaleTimeString()
+
+const displayTime = document.getElementById('displayTime')
+displayTime.innerHTML = presentTime
